1c7cda3712d2ed64107ecb60d30c953e39ee827b
nuggy875 <jinugpaeng@gmail.com>
2019-05-28 22:25:43 +0900

    get ip

--- server/server/routes/routes.js
+++ server/server/routes/routes.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
+    const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
+    console.log('ip:'+ip);
     res.send('This is PetComm, Welcome World!');
 });
 
