afe814a356b7ea0fc2cc6e96f6c751dd95005b0e
nuggy875 <jinugpaeng@gmail.com>
2019-07-25 16:16:37 +0900

    set IP

--- PetComm/app/src/main/java/com/example/petcomm/utils/Constants.java
+++ PetComm/app/src/main/java/com/example/petcomm/utils/Constants.java
@@ -2,8 +2,13 @@ package com.example.petcomm.utils;
 
 public class Constants {
     //public static final String BASE_URL = "http://localhost:8080/api/pk/";
+
     // with virtual Machine
-    public static final String BASE_URL = "http://10.0.2.2:8080/api/pk/";
+    // public static final String BASE_URL = "http://10.0.2.2:8080/api/pk/";
+
+    // PK Server IP
+    public static final String BASE_URL = "http://220.71.91.185/api/pk";
+
     public static final String EMAIL = "email";
     public static final String DOG = "selectedDog";
 
